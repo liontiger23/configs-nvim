e8b958fa54e0df0bb504b4a2d56158151237d8f7
Ivan Trepakov <liontiger23@gmail.com>
2024-05-01 18:15:31 +0700

    illuminate: disable visual highlighting because colorscheme handles it just fine

--- after/plugin/illuminate.lua
+++ after/plugin/illuminate.lua
@@ -7,6 +7,6 @@ require('illuminate').configure({
 })
 
 -- Enable visual highlighting
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+--vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+--vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+--vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
