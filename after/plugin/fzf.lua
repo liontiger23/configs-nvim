47dc39fd6d0819f16c566aa86a2cda56f8a2aaa0
Ivan Trepakov <liontiger23@gmail.com>
2024-12-23 15:30:37 +0700

    fzf: improve layout and add resume keymap

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,16 +1,25 @@
 local fzf_lua = require('fzf-lua')
 fzf_lua.setup {
-    keymap = {
-        fzf = {
-            -- Open all matches as quickfix window
-            ["ctrl-q"] = "select-all+accept",
-        },
+  defaults = {
+    formatter = "path.filename_first",
+  },
+  keymap = {
+    fzf = {
+      -- Open all matches as quickfix window
+      ["ctrl-q"] = "select-all+accept",
     },
-    lsp = {
-      includeDeclaration = false
-    },
+  },
+  lsp = {
+    includeDeclaration = false,
+    winopts = {
+      preview = {
+        layout = "vertical",
+      }
+    }
+  },
 }
 
+vim.keymap.set('n', '<leader>fr', fzf_lua.resume, { desc = "Resume previous fuzzy search" })
 vim.keymap.set('n', '<leader>ff', fzf_lua.files, { desc = "Fuzzy search files" })
 vim.keymap.set('n', '<leader>fg', fzf_lua.live_grep, { desc = "Fuzzy grep" })
 vim.keymap.set('n', '<leader>fb', fzf_lua.buffers, { desc = "Fuzzy search buffers" })
